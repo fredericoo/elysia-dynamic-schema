9dac33b60e8782fc02133992ef250d03e49e2893
freddie <rincofrederico@gmail.com>
2024-04-12 15:24:44 +0100

    poc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-import { Elysia } from "elysia";
+import { Elysia, error, t } from "elysia";
+import {Value} from "@sinclair/typebox/value";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+const schemas: Record<string, string> = {
+  'with-name': JSON.stringify(t.Object({ name: t.String() }))
+}
+
+const app = new Elysia()
+  .get("/", () => schemas)
+  .post("/schema/:id", ({body, params}) => {
+    const {schema } = body;
+    schemas[params.id] = schema;
+  }, {body: t.Object({
+    schema: t.String()
+  })})
+  .post("/validate/:schema", async ({params, body}) => {
+    const schemaString = schemas[params.schema];
+    if (!schemaString) return error(404, `Schema ${params.schema} not found`);
+
+    const schemaObj = JSON.parse(schemaString);
+    console.log(schemaObj);
+
+    try {
+      // Here we’d decode the schema, but as a JSON schema object it won}t work direclty with typebox.
+      return Value.Decode(schemaObj,body) ;
+    } catch (e) {
+      console.error(e)
+      return {ok: false}
+    }
+  })
+  .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
 );
